2e6bf0b82b1cab9ded4fecb5a8fc7d680859e28d
Nikolas Barwicki <nikolas.barwicki@gmail.com>
2020-06-18 12:59:08 +0200

    Add mongoose schema

---
+++ models/Beer.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const BeerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    unique: true,
+  },
+  brewery: {
+    type: String,
+    required: [true, 'Please add a brewery name'],
+  },
+  country: {
+    type: String,
+    required: [true, 'Please choose a country'],
+  },
+  style: {
+    type: String,
+    required: [true, 'Please choose a style'],
+  },
+  ABV: {
+    type: Number,
+  },
+  IBU: {
+    type: Number,
+  },
+  rating: {
+    type: Number,
+    min: [1, 'Rating must be at least 1'],
+    max: [5, 'Rating must can not be more than 5'],
+    required: [true, 'Please rate a beer'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Beer', BeerSchema);
